c6bd469d5e6488f0bd1f3ebc6ab2f2466dae2140
jordan-wei-taylor <jordanweitaylor@gmail.com>
2022-05-03 23:18:13 +0100

    completed up to rm

--- directory-demo.sh
+++ directory-demo.sh
@@ -10,7 +10,9 @@ function wait {
     echo -ne "\033[2K\r"
 }
 
-echo -e "lets print the current working directory with the following command:\n\n$ pwd"
+dir=$(pwd)
+
+echo -e "lets print the current working directory with the following command:\n\n$dir$ pwd"
 
 wait
 
@@ -18,7 +20,7 @@ pwd
 
 wait
 
-echo -e "lets print all files and folders in the current directory with:\n\n$ ls"
+echo -e "lets print all files and folders in the current directory with:\n\n$dir$ ls"
 
 wait 
 
@@ -26,7 +28,7 @@ ls
 
 wait
 
-echo -e "lets change our directory to $(pwd)/directory with:\n\n$ cd directory"
+echo -e "lets change our directory to $(pwd)/directory with:\n\n$dir$ cd directory"
 
 wait
 
@@ -36,7 +38,9 @@ echo -e "lets verify our working directory followed by it's contents with \"pwd\
 
 wait
 
-echo "$ pwd"
+dir=$(pwd)
+
+echo "$dir$ pwd"
 
 wait
 
@@ -44,10 +48,53 @@ pwd
 
 wait
 
-echo "$ tree -a"
+echo "$dir$ tree -a"
 
 wait 
 
 tree -a
 
-wait 
+wait
+
+echo -e "we know there are three folders in our current directory from the previous executing of \"tree -a\""
+echo -e "lets verify this by using the \"ls\" command:\n\n$dir$ ls"
+
+wait
+
+ls
+
+wait
+
+echo "notice how the \".c\" folder is missing?"
+echo "this is because files and folders that start with a \".\" are hidden in linux"
+echo -e "we can tell \"ls\" to show hidden objects by the following command:\n\n$dir$ ls -all"
+
+wait
+
+ls -all
+
+wait
+
+echo -e "lets try making a directory \"d\" as well as the subdirectory \"d/e\"\n\n$dir$ mkdir d\n$dir$ mkdir d/e"
+
+wait
+
+mkdir d
+mkdir d/e
+
+wait
+
+echo -e "we can create a blank txt file in the subdirectory with \n\n$dir$ touch d/e/1.txt"
+
+wait
+
+touch d/e/1.txt
+
+echo -e "and verify the current tree of our directory with \"tree -a\"\n\n$dir$ tree -a"
+
+wait
+
+tree -a
+
+exit 0
+
